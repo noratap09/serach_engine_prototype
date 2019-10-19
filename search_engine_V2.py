0221f41ee4a15d224007410e8f89960fbeba6040
noratap09 <35356878+noratap09@users.noreply.github.com>
2019-10-19 16:14:50 +0700

    Update search_engine_V2.py

--- search_engine_V2.py
+++ search_engine_V2.py
@@ -9,12 +9,14 @@ txt = my_file.read()
 
 json_obj = json.loads(txt)
 
+vocab_per_paragraph = 3700
+
 for i in json_obj.keys():
-    num = math.ceil(len(json_obj[i])/3700)
+    num = math.ceil(len(json_obj[i])/vocab_per_paragraph)
     pos_s = 0
     for j in range(0,num):
         print(i,":",j,"/",num)
-        data = json_obj[i][j*3700:(j+1)*3700]
+        data = json_obj[i][j*vocab_per_paragraph:(j+1)*vocab_per_paragraph]
         pos_e = pos_s+len(''.join(data))
         #print(pos_s,":",pos_e)
         doc = {
