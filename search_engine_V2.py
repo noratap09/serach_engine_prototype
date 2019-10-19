b10a3c2ff6990be4a037c464b2a3f754fe91d310
noratap09 <35356878+noratap09@users.noreply.github.com>
2019-10-19 16:09:14 +0700

    PP เอาอันนี้ไปรัน
    แก่ path ให้ถูกด้วย ถ้ามี test_search_engine_v2 ใน elasticsearch อยู่แล้วให้ลบออกก่อนแล้วค่อยรัน

--- search_engine_V2.py
+++ search_engine_V2.py
@@ -4,17 +4,17 @@ import math
 from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
-my_file = open("dict_id2txt_troken.json",'r',encoding = 'utf-8-sig')
+my_file = open("document/dict_id2txt_troken_up.json",'r',encoding = 'utf-8-sig')
 txt = my_file.read()
 
 json_obj = json.loads(txt)
 
 for i in json_obj.keys():
-    num = math.ceil(len(json_obj[i])/30)
+    num = math.ceil(len(json_obj[i])/3700)
     pos_s = 0
     for j in range(0,num):
         print(i,":",j,"/",num)
-        data = json_obj[i][j*30:(j+1)*30]
+        data = json_obj[i][j*3700:(j+1)*3700]
         pos_e = pos_s+len(''.join(data))
         #print(pos_s,":",pos_e)
         doc = {
@@ -25,6 +25,6 @@ for i in json_obj.keys():
             'end_pos': pos_e
             }
 
-        res = es.index(index="test_search_engine_v2", doc_type='tweet',body=doc)
+        res = es.index(index="test_search_engine_v2",id=str(i)+"_"+str(j), doc_type='tweet',body=doc)
 
         pos_s = pos_e+1
